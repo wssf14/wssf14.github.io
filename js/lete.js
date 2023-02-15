6bf88af4ba5ee7a28e858b03c3e451f7b3652dd1
wssf14 <996992870@qq.com>
2023-02-15 13:33:10 +0800

    Site updated: 2023-02-15 13:33:10

--- js/lete.js
+++ js/lete.js
@@ -1,14 +1,10 @@
-// 设置页脚博主会动的心
-$(document).ready(function(e){
-    $('.copyright').html('©2020 <i style="color:#FF6A6A;animation: announ_animation 0.8s linear infinite;" class="fa fa-heartbeat"></i> Lete乐特');
-})
 
 // 移除页脚样式
-var app = document.getElementById('footer');
-app.removeAttribute('id')
+// var app = document.getElementById('footer');
+// app.removeAttribute('id')
 
 /*添加图片top*/
-var top_up = "<img id='upj' class='upj' style='max-width: 1000%; transform: translate(-70px,-70px);' src='https://cdn.jsdelivr.net/gh/lete114/CDN/Use/up.gif' title='回到顶部' >";
+var top_up = "<img id='upj' class='upj' style='max-width: 300%; transform: translate(-85px,-70px);' src='https://gcore.jsdelivr.net/gh/wssf14/image/littlekeqingtouming.png' title='回到顶部' >";
 /*添加到返回顶部按钮下*/
 document.getElementById("go-up").innerHTML += top_up;
 
@@ -22,3 +18,34 @@ for (var i = 0; i < menus_item_child.length; i++) {
     menus_item_child[i].style.display = "none";
     menus_expand[i].className += " menus-closed";
 }
+
+
+// 动态心跳
+$(document).ready(function(e){
+    $('.copyright').html('©2023 - 2023 <i class="fa-fw fas fa-heartbeat fa-beat" style="color: #f92672;"></i> By Hongzhi');
+})
+
+$(document).ready(function(e){
+    show_date_time();
+})
+
+//本站运行时间
+function show_date_time(){
+    $('.framework-info').html('本站已运行<span id="span_dt_dt" style="color: #fff;"></span>');
+    window.setTimeout("show_date_time()", 1000);
+    BirthDay=new Date("2/13/2023 0:0:0");
+    today=new Date();
+    timeold=(today.getTime()-BirthDay.getTime());
+    sectimeold=timeold/1000
+    secondsold=Math.floor(sectimeold);
+    msPerDay=24*60*60*1000
+    e_daysold=timeold/msPerDay
+    daysold=Math.floor(e_daysold);
+    e_hrsold=(e_daysold-daysold)*24;
+    hrsold=Math.floor(e_hrsold);
+    e_minsold=(e_hrsold-hrsold)*60;
+    minsold=Math.floor((e_hrsold-hrsold)*60);
+    seconds=Math.floor((e_minsold-minsold)*60);
+    span_dt_dt.innerHTML='<font style=color:#afb4db>'+daysold+'</font> 天 <font style=color:#f391a9>'+hrsold+'</font> 时 <font style=color:#fdb933>'+minsold+'</font> 分 <font style=color:#a3cf62>'+seconds+'</font> 秒';
+    }
+    
